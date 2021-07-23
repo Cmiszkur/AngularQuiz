48bea6ffa83be5954c797305816f131e3e02c240
Cmiszkur <cmiszkur@gmail.com>
2021-07-23 16:40:08 +0200

    commit on merge-question-component

--- src/app/questions/multiple-choice-question/multiple-choice-question.component.ts
+++ src/app/questions/multiple-choice-question/multiple-choice-question.component.ts
@@ -17,19 +17,36 @@ export class MultipleChoiceQuestionComponent implements OnInit {
     incorrect_answers: ['']
   }
   @Output() newAnswerEmitter = new EventEmitter<[string, boolean]>()
+  answers: string[] = ['']
 
   constructor() { }
 
   ngOnInit(): void {
+    let answers = this.data.incorrect_answers.concat(this.data.correct_answer)
+    let shuffledAnswers = this.shuffleAnswers(answers)
+    this.answers = shuffledAnswers
   }
 
-  addNewAnswer(value: string) {
+  addNewAnswer(value: string): void {
     let question = this.data.question
     if(value === this.data.correct_answer){
       this.newAnswerEmitter.emit([question, true])
     }else{
       this.newAnswerEmitter.emit([question, false])
     }
+  }
+
+  shuffleAnswers(answers: string[]): string[] {
+    let currentIndex = answers.length, randomIndex
+
+    while(0 !== currentIndex){
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex--
+
+      [answers[currentIndex], answers[randomIndex]] = [answers[randomIndex], answers[currentIndex]] 
+    }
+
+    return answers
   }
 
 }
